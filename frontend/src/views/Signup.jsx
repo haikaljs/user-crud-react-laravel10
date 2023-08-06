461e5fc770c6586692f35c4e4a483a56b2eff2f4
haikaljs <haikaljavascript@gmail.com>
2023-08-06 20:17:49 +0800

    Send data using axios from register page

--- frontend/src/views/Signup.jsx
+++ frontend/src/views/Signup.jsx
@@ -1,11 +1,15 @@
 import { useRef } from "react";
 import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+import useStateContext from "../contexts/ContextProvider.jsx";
 
 const Signup = () => {
     const nameRef = useRef();
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmationRef = useRef();
+
+    const { setUser, setToken } = useStateContext();
 
     const onSubmit = (ev) => {
         ev.preventDefault();
@@ -16,7 +20,18 @@ const Signup = () => {
             password_confirmation: passwordConfirmationRef.current.value,
         };
 
-        
+        axiosClient
+            .post("/signup", payload)
+            .then(({ data }) => {
+                setUser(data.user);
+                setToken(data.token);
+            })
+            .catch((err) => {
+                const response = err.response;
+                if (response && response.status === 422) {
+                  console.log(response.data.errors) 
+                }
+            });
     };
 
     return (
