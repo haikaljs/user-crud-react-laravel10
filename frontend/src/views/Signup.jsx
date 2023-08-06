aad67e65e7552e66576ab925e57b1a2f33b19aa4
haikaljs <haikaljavascript@gmail.com>
2023-08-06 19:01:26 +0800

    Design signup page

--- frontend/src/views/Signup.jsx
+++ frontend/src/views/Signup.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+    };
 
-export default Signup
+    return (
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Signup for free</h1>
+                    <input type="email" placeholder="Full Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                    <input
+                        type="password"
+                        placeholder="Password Confirmation"
+                    />
+                    <button className="btn btn-block">Sign up</button>
+                    <p className="message">
+                        Already Registered? <Link to="/login">Sign in</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
