4864766d7b4e911b9767c9df15eed28900185b55
haikaljs <haikaljavascript@gmail.com>
2023-08-06 20:07:28 +0800

    Setup useRef for all input in register page

--- frontend/src/views/Signup.jsx
+++ frontend/src/views/Signup.jsx
@@ -1,8 +1,22 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+
     const onSubmit = (ev) => {
         ev.preventDefault();
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        };
+
+        
     };
 
     return (
@@ -10,10 +24,19 @@ const Signup = () => {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Signup for free</h1>
-                    <input  placeholder="Full Name" />
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Password" />
+                    <input ref={nameRef} placeholder="Full Name" />
                     <input
+                        ref={emailRef}
+                        type="email"
+                        placeholder="Email Address"
+                    />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        ref={passwordConfirmationRef}
                         type="password"
                         placeholder="Password Confirmation"
                     />
