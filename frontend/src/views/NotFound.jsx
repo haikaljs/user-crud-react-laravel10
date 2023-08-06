e5c696bf86e956926646356dfca4d19a42e01925
haikaljs <haikaljavascript@gmail.com>
2023-08-06 08:49:09 +0800

    Create default & guest layout

--- frontend/src/views/NotFound.jsx
+++ frontend/src/views/NotFound.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const NotFound = () => {
-    return <div>404 - Page Not Found</div>;
+    return (
+        <div
+            style={{
+                minHeight: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+             
+            }}
+        >
+            <h1>404 - Page Not Found</h1>
+        </div>
+    );
 };
 
 export default NotFound;
