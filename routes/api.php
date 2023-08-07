6121ca98347da5f14d5ddfb791769b7cb9791634
haikaljs <haikaljavascript@gmail.com>
2023-08-07 16:42:08 +0800

    UserResource api routes

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::apiResource('/users', UserController::class);
 });
 
 
