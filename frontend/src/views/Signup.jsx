e81e6207187ba35504a60baf15beab1be577a2c2
haikaljs <haikaljavascript@gmail.com>
2023-08-07 11:20:59 +0800

    Display validation error to user

--- frontend/src/views/Signup.jsx
+++ frontend/src/views/Signup.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import axiosClient from "../axios-client.js";
 import { useStateContext } from "../contexts/ContextProvider.jsx";
@@ -8,6 +8,8 @@ const Signup = () => {
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmationRef = useRef();
+
+    const [errors, setErrors] = useState(null);
 
     const { setUser, setToken } = useStateContext();
 
@@ -28,11 +30,13 @@ const Signup = () => {
             .then(({ data }) => {
                 setUser(data.user);
                 setToken(data.token);
+                console.log("Success signup");
             })
             .catch((err) => {
                 const response = err.response;
                 if (response && response.status === 422) {
-                    console.log(response.data.errors);
+                    // console.log(response.data.errors);
+                    setErrors(response.data.errors);
                 }
             });
     };
@@ -42,6 +46,13 @@ const Signup = () => {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Signup for free</h1>
+                    {errors && (
+                        <div className="alert">
+                            {Object.keys(errors).map((key) => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    )}
                     <input ref={nameRef} placeholder="Full Name" />
                     <input
                         ref={emailRef}
