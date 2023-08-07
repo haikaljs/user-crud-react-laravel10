eb0f43f183945252ff79c97123fc013a25f7140a
haikaljs <haikaljavascript@gmail.com>
2023-08-07 08:43:53 +0800

    Signup, Signin, logout fuctionality

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,22 +2,53 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
+
 use App\Http\Requests\SignupRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request){
-        
-    }
+   
 
     public function signup(SignupRequest $request){
+        $data = $request->validated();
+        $user = User::create([
+           "name" => $data["name"],
+           "email" => $data["email"],
+           "password" => bcrypt($data["password"]),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        // return response([
+        //     "user" => $user,
+        //     "token" => $token
+        // ]);
+
+        return response(compact('user', 'token'));
+    }
+
+    public function login(LoginRequest $request){
+      $credentials = $request->validated();
+      if(!Auth::attempt($credentials)){
+        return response([
+            "message" => "Provided email addresses or password is incorrect"
+        ]);
+      }
+
+      $user = Auth::user();
+      $token = $user->createToken("main")->plainTextToken;
+      return response(compact('user', 'token'));
 
     }
 
     public function logout(Request $request){
-
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response("", 204);
     }
 }
